65350f01395fa42b060e722c56c47d23df280d73
adityabedekar17 <abedekar@ucsc.edu>
2025-02-07 12:50:56 -0800

    v1 logic for testbench

--- dv/picorv_uart_tb.sv
+++ dv/picorv_uart_tb.sv
@@ -1,3 +1,4 @@
+`timescale 1ns/1ps
 module picorv_uart_tb
     import config_pkg::*;
     import dv_pkg::*;
@@ -6,6 +7,39 @@ module picorv_uart_tb
 picorv_uart_runner picorv_uart_runner ();
 
 initial begin
+    picorv_uart_runner.rx_i = 1'b1; // idle?
+    // li x1,1
+    //li x2,2
+    // add x3,x1,x2 // check at x3 whether i can see result is 3
+    // to check whether it is really 3 im guessing i have to look somewhere in the RISC-V IP
+    
+    picorv_uart_runner.picorv_uart.ram_inst
+   
+
+    repeat(20) @(posedge runner.clk_i) begin
+      if (picorv_uart_runner.picorv_uart.mem_valid) && (picorv_uart_runner.picorv_uart.mem_ready) begin // ready valid
+        $display("Time=%0t: Memory Access @ %h = %h", 
+                $time, 
+                picorv_uart_runner.picorv_uart.mem_addr,
+                picorv_uart_runner.picorv_uart.mem_rdata);
+      end
+    end
+    
+
+     // Waveform dump
+  initial begin
+    $dumpfile("pico_uart_tb.vcd");
+    $dumpvars(0, pico_uart_tb);
+  end
+
+
+    
+    
+    
+    
+    
+    
+    /*
     $dumpfile( "dump.fst" );
     $dumpvars;
     $display( "Begin simulation." );
@@ -15,6 +49,8 @@ initial begin
     picorv_uart_runner.reset();
 
     $display( "End simulation." );
-    $finish;
+    $finish;*/
 end
+
+
 endmodule
