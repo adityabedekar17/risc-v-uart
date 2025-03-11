36c6c7497ec04c4ef28d083001ab0da675cb952d
adityabedekar17 <abedekar@ucsc.edu>
2025-03-10 17:54:35 -0700

    updated rtl filelist

--- rtl/rtl.f
+++ rtl/rtl.f
@@ -6,10 +6,10 @@ rtl/config_pkg.sv
 rtl/picorv_uart.sv
 rtl/uart_ram.sv
 rtl/ram_1r1w_sync.sv
-
 third_party/alexforencich_uart/rtl/uart.v
 third_party/alexforencich_uart/rtl/uart_rx.v
 third_party/alexforencich_uart/rtl/uart_tx.v
 third_party/picorv32/picorv32.v
 
 third_party/hpdcache/rtl/src/hpdcache.sv
+rtl/hpdcache_wrapper.sv
